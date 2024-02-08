7e734323ed5ed780e0881ab04e225334e1c2d0a6
Marcelo Borges <marcelo.b.org@outlook.com>
2024-02-08 13:56:31 +0000

    comentando código que não funcionou

--- models/newshippers.sql
+++ models/newshippers.sql
@@ -1,9 +1,10 @@
-select 
-sh.shipper_id,
-se.shipper_email
-from 
-{{source("sources",'shippers')}} sh
-left join
-{{ref("shippers_emails")}} se
-on 
-(sh.shipper_id = se.shipper_id)
+--Este código de alguma forma não funcionou, o arquivo csv não está sendo alcançavel. 
+-- select 
+-- sh.shipper_id,
+-- se.shipper_email
+-- from 
+-- {{source("sources",'shippers')}} sh
+-- left join
+-- {{ref("shippers_emails")}} se
+-- on 
+-- (sh.shipper_id = se.shipper_id)
